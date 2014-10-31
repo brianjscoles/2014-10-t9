14a254a58a080c668b17d73f029fbca018519fa1
Hack Reactor Students <accounts@hackreactor.com>
2014-10-31 13:22:16 -0700

    add scaffolding and bit of functionality

--- t9.js
+++ t9.js
@@ -1,1 +1,37 @@
 //prefix tree maker!
+
+// lexicon should be an array of strings.
+
+var makePrefixTree = function(){
+  var prefixTree = Object.create(ptMethods);
+
+
+  return prefixTree;
+}
+
+
+
+
+
+var makeNode = function(value){
+  var node = {};
+  node.value = value;
+  node.children = [];
+  node.isWord = false;
+}
+
+var ptMethods = {};
+
+ptMethods.buildTree = function(lexicon){
+  for (var w = 0; w < lexicon.size; w++){
+    var letters = lexicon[w].split('');
+    for(var l = 0; l < letters.size; l++){
+      this.addLettertoTree(l);
+    }
+  }
+}
+
+ptMethods.addLettertoTree = function(l){
+
+}
+
